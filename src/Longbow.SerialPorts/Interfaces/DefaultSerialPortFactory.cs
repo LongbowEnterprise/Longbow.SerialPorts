bf1ce2f536106f6f45e07c8cea57215bf70d1203
Argo Zhang <argo@live.ca>
2025-09-13 10:31:53 +0800

    feat: 增加 ISerialPortFactory 工厂类

---
+++ src/Longbow.SerialPorts/Interfaces/DefaultSerialPortFactory.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+using System.Collections.Concurrent;
+
+namespace Longbow.SerialPorts;
+
+sealed class DefaultSerialPortFactory : ISerialPortFactory
+{
+    private readonly ConcurrentDictionary<string, ISerialPortClient> _pool = new();
+
+    public ISerialPortClient GetOrCreate(string? name = null, Action<SerialPortOptions>? valueFactory = null)
+    {
+        return string.IsNullOrEmpty(name) ? CreateClient(valueFactory) : _pool.GetOrAdd(name, key => CreateClient(valueFactory));
+    }
+
+    private static DefaultSerialPortClient CreateClient(Action<SerialPortOptions>? valueFactory = null)
+    {
+        var options = new SerialPortOptions();
+        valueFactory?.Invoke(options);
+        return new DefaultSerialPortClient(options);
+    }
+
+    public ISerialPortClient? Remove(string name)
+    {
+        ISerialPortClient? client = null;
+        if (_pool.TryRemove(name, out var c))
+        {
+            client = c;
+        }
+        return client;
+    }
+
+    private async ValueTask DisposeAsync(bool disposing)
+    {
+        if (disposing)
+        {
+            // 释放托管资源
+            foreach (var socket in _pool.Values)
+            {
+                await socket.DisposeAsync();
+            }
+            _pool.Clear();
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+}
