854dbd56d17a024a620043c558840568107d8d91
Argo Zhang <argo@live.ca>
2025-09-13 10:31:53 +0800

    feat: 增加 ISerialPortFactory 工厂类

---
+++ src/Longbow.SerialPorts/Interfaces/ISerialPortFactory.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+namespace Longbow.SerialPorts;
+
+/// <summary>
+/// ITcpSocketFactory Interface
+/// </summary>
+public interface ISerialPortFactory : IAsyncDisposable
+{
+    /// <summary>
+    /// 生成 <see cref="ISerialPortClient"/> 实例方法
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="valueFactory"></param>
+    /// <returns></returns>
+    ISerialPortClient GetOrCreate(string? name = null, Action<SerialPortOptions>? valueFactory = null);
+
+    /// <summary>
+    /// 移除指定名称 <see cref="ISerialPortClient"/> 实例方法
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    ISerialPortClient? Remove(string name);
+}
